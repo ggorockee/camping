ad5f7af2a57375da33933e7e084b42beb69cbc1b
ggorockee <ggorockee@gmail.com>
2025-07-15 11:46:07 +0900

    file 이름 변경

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,9 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
-import CampsiteNew from '../views/CampSiteNew.vue'
+import CampSiteNew from '../views/CampSiteNew.vue'
 import RegisterView from '../views/RegisterView.vue'
-import CampsiteDetailView from '../views/CampSiteDetailView.vue'
+import CampSiteDetailView from '../views/CampSiteDetailView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,12 +26,12 @@ const router = createRouter({
     {
       path: '/new',
       name: 'new',
-      component: CampsiteNew,
+      component: CampSiteNew,
     },
     {
       path: '/campsites/:id', // :id 부분이 동적으로 변경됩니다.
       name: 'campsite-detail', // 이 이름으로 링크를 쉽게 만들 수 있습니다.
-      component: CampsiteDetailView,
+      component: CampSiteDetailView,
     },
   ],
 })
